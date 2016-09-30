87dfcb1817346712ea48433e869f4491330105b3
Yoga Lin <blueandhack@gmail.com>
2016-09-30 04:09:51 -0700

    added homework 03

---
+++ homework03/hw03.s
@@ -0,0 +1,23 @@
+.data
+mask: .word 0xF0F0F0F0
+.text
+main:
+
+	addi $s1, $zero, -1	
+	andi $s2, $s1, 0xffff
+	add $a0, $zero, $s2
+	addi $v0, $zero, 1
+	syscall
+	
+	addi	$a0, $zero, 0xa		# print_char('\n')
+	add	$v0, $zero, 11
+	syscall
+	
+	ori $s3, $zero, 0xffff
+	add $a0, $zero, $s3
+	addi $v0, $zero, 1
+	syscall
+	
+	addi	$a0, $zero, 0xa		# print_char('\n')
+	add	$v0, $zero, 11
+	syscall
