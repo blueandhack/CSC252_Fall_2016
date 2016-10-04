b3d590ab964b2d62ba6f99a44d24586c4f550e12
Yoga Lin <blueandhack@gmail.com>
2016-10-03 17:14:30 -0700

    updated project 03

--- project03/proj03.s
+++ project03/proj03.s
@@ -1,5 +1,12 @@
 .data
-invalidMode:	.asciiz 	"Invalid Mode."
+
+mode:
+	.byte	1
+
+invalidMode:
+	.asciiz 	"Invalid Mode."
+
+
 .text
 main:
 	# Function prologue -- even main has one
@@ -11,6 +18,34 @@ main:
 loadSomehings:
 	la	$t0, mode	# $t0 = &mode
 	lb	$s0, 0($t0)	# $s0 = mode
+	
+ifEqualsOne:
+	beq	$s0, 0x1, printInvalidMode
+ifEqualsTwo:
+	beq	$s0, 0x2, printStringMode
+ifEqualsThree:
+	beq	$s0, 0x3, doubleIndexMode
+ifEqualsFour:
+	beq	$s0, 0x4, compareAndSwapTwoString
+	j	done
+	
+printInvalidMode:
+	la	$t0, invalidMode	# print "Invalid Mode."
+	add	$a0, 0($t0)
+	addi	$v0, $zero, 4
+	syscall
+	
+	addi	$a0, $zero, 0xa		# print_char('\n')
+	add	$v0, $zero, 11
+	syscall
+	
+printStringMode:
+
+doubleIndexMode:
+
+compareAndSwapTwoString:   
+        
+        
         
 done:
 	# Epilogue for main -- restore stack & frame pointers and return
