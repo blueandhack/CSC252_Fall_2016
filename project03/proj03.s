8abbb5e051ea54e41c2b00dbf53ac4c6674889dc
Yoga Lin <blueandhack@gmail.com>
2016-10-06 18:51:04 -0700

    updated project 03

--- project03/proj03.s
+++ project03/proj03.s
@@ -51,15 +51,15 @@ STR_CONST__twelve:
 STR_CONST__thirteen:
 	.asciiz "thirteen"
 STR_CONST__fourteen:
-	.asciiz "fourteen"
+	.asciiz "AAABVVV"
 STR_CONST__fifteen:
-	.asciiz "fifteen"
+	.asciiz "AAAB"
 
 
 s1:
-	.byte   14
-s2:
 	.byte   15
+s2:
+	.byte   14
 c:
 	.half   0
 	
@@ -67,12 +67,19 @@ debug:
 	.asciiz "Debug"
 	
 # ----- STUDENT CODE BELOW -----	
+# proj03.s
+#
+# CSc 252 Fall 16 - Project 3
+#
+# Name: Yujia Lin
+#
+# Prints Invalid Mode, String, char, and sort two Strings
+
 
 .data 
 
 invalidMode:
 	.asciiz 	"Invalid Mode."
-
 
 .text
 
@@ -84,48 +91,59 @@ main:
         addiu	$fp, $sp, 20	# setup main's frame pointer
         
 	# LOAD THE COMMON VARIABLES
-	la	$t0, mode	# $t0 = &mode
-	lb	$s0, 0($t0)	# $s0 = mode
+	# 
+	# These will be used at various places in the program below.
+	# 	$s0 = mode
+	#	$s1 = strings
+	#	$s2 = numStrs
+	#	$s3 = s1
+	#	$s4 = s2
+	#	$s5 = c
 	
-	la	$s1, strings	# $s1 = &strings
+	la	$t0, mode				# $t0 = &mode
+	lb	$s0, 0($t0)				# $s0 = mode
 	
-	la	$t0, numStrs	# $t0 = &numStrs
-	lb	$s2, 0($t0)	# $s2 = numStrs
+	la	$s1, strings				# $s1 = &strings
 	
-	la	$t0, s1		# $t0 = &s1
-	lb	$s3, 0($t0)	# $s3 = s1
+	la	$t0, numStrs				# $t0 = &numStrs
+	lb	$s2, 0($t0)				# $s2 = numStrs
 	
-	la	$t0, s2		# $t0 = &s2
-	lb	$s4, 0($t0)	# $s4 = s2
+	la	$t0, s1					# $t0 = &s1
+	lb	$s3, 0($t0)				# $s3 = s1
 	
-	la	$t0, c
-	lh	$s5, 0($t0)	# $s5 = c
+	la	$t0, s2					# $t0 = &s2
+	lb	$s4, 0($t0)				# $s4 = s2
+	
+	la	$t0, c					# $t0 = &c
+	lh	$s5, 0($t0)				# $s5 = c
 	
 ifEqualsOne:
-	beq	$s0, 0x1, printInvalidMode
+	beq	$s0, 0x1, printInvalidMode		# if(mode == 1) jump printInvalidMode
 ifEqualsTwo:
-	beq	$s0, 0x2, printStringMode
+	beq	$s0, 0x2, printStringMode		# if(mode == 2) jump printStringMode
 ifEqualsThree:
-	beq	$s0, 0x3, doubleIndexMode
+	beq	$s0, 0x3, doubleIndexMode		# if(mode == 3) jump doubleIndexMode
 ifEqualsFour:
-	beq	$s0, 0x4, compareAndSwapTwoString
-	j	done
+	beq	$s0, 0x4, compareAndSwapTwoString	# if(mode == 4) jump compareAndSwapTwoString
+	
+	j	done					# if(mode != 1 && mode != 2 && mode != 3 && mode != 4) jump done
 	
 printInvalidMode:
 	# Print "Invalid Mode." When Mode is 1
 	# 
 	# if(mode == 1)
 	# 	printf("Invalid Mode.\n");
+	#
 	
 	la	$a0, invalidMode	# print "Invalid Mode."
-	addi	$v0, $zero, 4
-	syscall
+	addi	$v0, $zero, 4		#
+	syscall				#
 	
 	addi	$a0, $zero, 0xa		# print_char('\n')
-	add	$v0, $zero, 11
-	syscall
+	add	$v0, $zero, 11		#
+	syscall				#
 	
-	j	done
+	j	done			# jump done
 	
 printStringMode:
 	# Print String Array
@@ -133,40 +151,51 @@ printStringMode:
 	# for(i = 0; i < numStrs; i++){
 	#	printf("%s", strings[i]);
 	# }
+	# 
+	# In this loop, we have additional variables:
+	# $t1 - i
+	# $t2 - temporaries
+	# $t3 - strings[i]
 	
-	addi	$t1, $zero, 0		# i = 0
+	addi	$t1, $zero, 0				# i = 0
 
 printStringModeLoop:
-	beq	$s2, $t1, printStringModeLoopEnd 
-	add	$t0, $t1, $t1				# i = i * 2
-	add	$t0, $t0, $t0				# i = i * 4
-	add	$t2, $s1, $t0				# $t2 = &string[i]
+	beq	$s2, $t1, printStringModeLoopEnd 	# if (i == numStrs) jump printStringModeLoopEnd
+	add	$t0, $t1, $t1				# $t0 = i * 2
+	add	$t0, $t0, $t0				# $t0 = i * 4
+	add	$t2, $s1, $t0				# $t2 = &string[$t0]
 	
 	lw	$t3, 0($t2)				# $t3 = string[i]
-	add	$a0, $zero, $t3				# Print string[i]
+	add	$a0, $zero, $t3				# print string[i]
 	addi	$v0, $zero, 4				#
-	syscall
+	syscall						#
 	
 	addi	$a0, $zero, 0xa				# print_char('\n')
-	add	$v0, $zero, 11
-	syscall
+	add	$v0, $zero, 11				#
+	syscall						#
 	
-	addi	$t1, $t1, 1				# i = i + 1
-	j	printStringModeLoop			# Jump to printStringModeLoop
+	addi	$t1, $t1, 1				# i ++
+	j	printStringModeLoop			# jump to printStringModeLoop
 	
 printStringModeLoopEnd:
-	j	done
+	j	done					# jump done
 	
 	
 doubleIndexMode:
-	# Print index of String Array and index of char in String
-	# printf("%c", string[s1][c]);
+	# Print a char that from a String
+	# char s[] = string[s1];
+	# char *pointer = s
+	# printf("%c", *(pointer + c));
 	# 
+	# We have additional variables:
+	# $t1 - i
+	# $t2 - &string[i]
+	# $t3 - string[i]
 	
-	addi	$t1, $s3, 0
+	addi	$t1, $s3, 0				# i = s1
 	
-	add	$t0, $t1, $t1
-	add	$t0, $t0, $t0
+	add	$t0, $t1, $t1				# 
+	add	$t0, $t0, $t0				#
 	add	$t2, $s1, $t0				# $t2 = &string[s1]
 	
 	lw	$t3, 0($t2)				# $t3 = string[s1]
@@ -177,106 +206,130 @@ doubleIndexMode:
 	syscall
 	
 	addi	$a0, $zero, 0xa				# print_char('\n')
-	add	$v0, $zero, 11
-	syscall
+	add	$v0, $zero, 11				#
+	syscall						#
 	
-	j	done
+	j	done					# jump done
 	
-compareAndSwapTwoString:   
+compareAndSwapTwoString:
+	# Compare two String and sort them
+        # int strcmp(char *str1, char *str2)
+	# {
+	# 	int i = 0;
+	#
+	# 	while (str1[i] != ’\0’ && str2[i] != ’\0’)
+	# 	{
+	# 		// do we have a mismatched character?
+	# 		if (str1[i] < str2[i])
+	# 			return -1;
+	# 		if (str1[i] > str2[i])
+	# 			return 1;
+	# 		i++;
+	# 	}
+	#
+	# 	// str1 or str2 (or both) have hit the end of their
+	# 	// strings; they were identical up to that point.
+	#
+	# 	if (str2[i] != ’\0’)
+	# 		return -1; // str1 is shorter than str2
+	# 	if (str1[i] != ’\0’)
+	# 		return 1; // str2 is shorter than str1
+	# 	// exact match!
+	#
+	# 	return 0;
+	# }
+	#
+	# We have additional variables:
+	# $t0 - i
+	# $t3 - strings[s1]
+	# $t4 - strings[s2]
+	# $t5 - strings[s1][i]
+	# $t6 - strings[s2][i]
+	
         addi	$t1, $s3, 0				# i = s1
         
         add	$t0, $t1, $t1				#
         add	$t0, $t0, $t0				#
-        add	$t2, $s1, $t0				#
+        add	$t2, $s1, $t0				# $t2 = &string[s1]
         
-        lw	$t3, 0($t2)				# $t3 = string[s1]
-        
-        # add	$a0, $zero, $t3
-        # addi	$v0, $zero, 4
-        # syscall
-        
-        # addi	$a0, $zero, 0xa				# print_char('\n')
-	# add	$v0, $zero, 11
-	# syscall
+        lw	$t3, 0($t2)				# $t3 = strings[s1]
         
         addi	$t1, $s4, 0				# i = s2
         
         add	$t0, $t1, $t1				#
         add	$t0, $t0, $t0				#
-        add	$t2, $s1, $t0				#
+        add	$t2, $s1, $t0				# $t2 = &strings[s2]
         
-        lw	$t4, 0($t2)				# $t4 = string[s2]
-	        
-        # add	$a0, $zero, $t4
-        # addi	$v0, $zero, 4
-        # syscall
+        lw	$t4, 0($t2)				# $t4 = strings[s2]
         
-        # $t3 = s1  $t4 = s2
         addi	$t0, $zero, 0				# i = 0
         
 compareLoop:
-	add	$t1, $t3, $t0
-	lb	$t5, 0($t1)
+	add	$t1, $t3, $t0				#
+	lb	$t5, 0($t1)				# $t5 = strings[s1][i]
 	
-	add	$t1, $t4, $t0
-	lb	$t6, 0($t1)
+	add	$t1, $t4, $t0				#
+	lb	$t6, 0($t1)				# $t6 = strings[s2][i]
         
-        beq	$t5, $zero, compareLoopEnd
-        beq	$t6, $zero, compareLoopEnd
+        beq	$t5, $zero, compareLoopEnd		# if(strings[s1][i] == '\0') jump compareLoopEnd
+        beq	$t6, $zero, compareLoopEnd		# if(strings[s2][i] == '\0') jump compareLoopEnd
         
-        slt	$t1, $t5, $t6				# if str1[i] < str2[i]
-        bne	$t1, $zero, s1smalls2
-        slt	$t1, $t6, $t5				# if str1[i] > str2[i]
-        bne	$t1, $zero, s2smalls1
+        slt	$t1, $t5, $t6				# $t1 = strings[s1][i] < strings[s2][i]
+        bne	$t1, $zero, s1smalls2			# if(strings[s1][i] < strings[s2][i]) jump s1smalls2
+        
+        slt	$t1, $t6, $t5				# $t1 = strings[s2][i] < strings[s1][i]
+        bne	$t1, $zero, s2smalls1			# if(strings[s1][i] > strings[s2][i]) jump s2smalls1
         
         addi	$t0, $t0, 1				# i ++
         
-        j	compareLoop
+        j	compareLoop				# jump compareLoop
         
 compareLoopEnd:
+	bne	$t5, $zero, s2smalls1			# if(strings[s1][i] == '\0') jump s2smalls1
+	bne	$t6, $zero, s1smalls2			# if(strings[s2][i] == '\0') jump s1smalls2
 	
-	j	printStringMode
+	j	printStringMode				# jump printStringMode
+	
 s1smalls2:
-	slt	$t1, $s3, $s4				# s1 < s2
-	bne	$t1, $zero, compareEnd
-	# s1 > s2
-	# $t3 address for str1 $t4 address for str2
+	slt	$t1, $s3, $s4				# $t1 = s1 < s2
+	bne	$t1, $zero, compareEnd			# if(s1 < s2) jump compareEnd
+	
+	# if(s1 > s2) do below
 	addi	$t1, $s3, 0				# i = s1
         
         add	$t0, $t1, $t1				#
         add	$t0, $t0, $t0				#
         add	$t2, $s1, $t0				#
-        sw	$t4, 0($t2)
+        sw	$t4, 0($t2)				# strings[s2] pointer &strings[s1]
         
         addi	$t1, $s4, 0				# i = s2
         
         add	$t0, $t1, $t1				#
         add	$t0, $t0, $t0				#
         add	$t2, $s1, $t0				#
-	sw	$t3, 0($t2)
+	sw	$t3, 0($t2)				# strings[s1] pointer &strings[s2]
 	
 s2smalls1:	
-	slt	$t1, $s4, $s3				# s2 < s1
-	bne 	$t1, $zero, compareEnd
-	# s1 < s2
+	slt	$t1, $s4, $s3				# $t1 = s2 < s1
+	bne 	$t1, $zero, compareEnd			# if(s2 < s1) jump comapreEnd
+	
+	# if(s2 > s1) do below
 	addi	$t1, $s3, 0				# i = s1
         
         add	$t0, $t1, $t1				#
         add	$t0, $t0, $t0				#
         add	$t2, $s1, $t0				#
-        sw	$t4, 0($t2)
+        sw	$t4, 0($t2)				# strings[s2] pointer &strings[s1]
         
         addi	$t1, $s4, 0				# i = s2
         
         add	$t0, $t1, $t1				#
         add	$t0, $t0, $t0				#
         add	$t2, $s1, $t0				#
-	sw	$t3, 0($t2)
-	
-	
-	
+	sw	$t3, 0($t2)				# strings[s1] pointer &strings[s2] 
+		
 compareEnd:
-        j	printStringMode	
+        j	printStringMode				# jump printStringMode
         
 done:
 	# Epilogue for main -- restore stack & frame pointers and return
